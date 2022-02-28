640119a439302de4c978970fae4f4146f8cf1900
André <ruland@outlook.de>
2022-02-28 13:05:44 +0100

    HalloWCF

---
+++ HalloWCF/HalloWCF.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloWCF.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WCF Client");
+
+            var client = new ServiceReference1.Service1Client();
+
+            var result = client.GetData(8342756);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
